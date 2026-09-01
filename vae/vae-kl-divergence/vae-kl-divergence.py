eba26477127409e5c70a0c820188f797d96e1e5c
tourmii <134634435+tourmii@users.noreply.github.com>
2026-09-01 22:51:47 +0700

    Solve vae-kl-divergence

--- vae/vae-kl-divergence/vae-kl-divergence.py
+++ vae/vae-kl-divergence/vae-kl-divergence.py
@@ -6,5 +6,6 @@ def kl_divergence(mu: np.ndarray, log_var: np.ndarray) -> float:
     """
     # Your implementation here
     var = np.exp(log_var)
-    kl = -0.5 * np.sum(1 + log_var - (mu ** 2) - var, axis=1).mean(axis=0)
+    kl_per_sample = -0.5 * np.sum(1 + log_var - (mu ** 2) - var, axis=1)
+    kl = kl_per_sample.mean(axis=0)
     return kl
